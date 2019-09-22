687e7434ba964fd0aeb8a219177fc617c2d0542e
Rusavskiy Vitaliy <rusavskiy@gmail.com>
2019-09-23 00:20:17 +0300

    auth

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
+use Illuminate\Support\Facades\Route;
+
+Route::get(
+    '/', function () {
     return view('welcome');
-});
+}
+);
+
+Route::get('/test', 'TestController@index');
+
+Route::resource('photo', 'PhotoController', []);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
